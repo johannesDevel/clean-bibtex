97662a751bf651c278e670d72101b1432c684492
johannes <e1346716@student.tuwien.ac.at>
2019-09-01 12:51:07 +0200

    first commit

---
+++ src/AppStart.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+
+class AppStart extends Component {
+
+  render() {
+    return (
+      <div className="start">
+        <div className="start-inputs">
+          <input></input>
+          <button>Upload BibTex file</button>
+        </div>
+        Clean BibTex
+      </div>
+    )
+  }
+
+
+}
+export default AppStart;
