0ec806843eb521c047342ac2bd1948624a42db35
johannes <e1346716@student.tuwien.ac.at>
2019-12-04 13:02:00 +0100

    set mock data as state in App.js

--- src/App.js
+++ src/App.js
@@ -3,33 +3,63 @@ import "./App.css";
 import AppStart from "./AppStart";
 import * as BibtexAPI from "./utils/BibtexAPI";
 import AnalyzeErrors from "./AnalzyeErrors";
+import MockData from './utils/MockData';
 
 class App extends Component {
   state = {
-    entries: [],
-    categories: {
-      capitalization: {
-        titleCase: [],
-        sentenceCase: [],
-        caseNotFound: []
+    bibtexText: "",
+    entries: [
+      {
+        id: 1,
+        author: "Milton Abramowitz and Irene A. Stegun",
+        title: "Handbook of mathematical Functions with Tables",
+        capitalization: "caseNotFound",
+        correctionTitleCase: "Handbook of Mathematical Functions With Tables",
+        correctionSentenceCase: "Handbook of mathematical functions with tables"
       },
-      authorName: {},
-      mandatoryFields: {},
-    },
-    corrections: {
-      capitalization: [],
-      authorName: [],
-      mandatoryFields: []
-    },
-    optionsCheckboxes: []
+      {
+        id: 2,
+        author: "Bex, Floris and Villata, Serena",
+        title:
+          "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference",
+          capitalization: "sentenceCase",
+          correctionTitleCase: "Legal Knowledge and Information Systems: JURIX 2016: the Twenty-Ninth Annual Conference",
+          correctionSentenceCase: "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference"
+      },
+      {
+        id: 3,
+        author: "Winkels, Raboud Maarten",
+        title: "Section Structure of Dutch Court Judgments",
+        capitalization: "titleCase",
+        correctionTitleCase: "Section Structure of Dutch Court Judgments",
+        correctionSentenceCase: "section structure of dutch court judgments"
+      },
+      {
+        id: 4,
+        author: "Trompper, Maarten and Winkels, Raboud",
+        title:
+          "Automatic Assignment of Section Structure to Texts of Dutch Court Judgments",
+          capitalization: "titleCase",
+          correctionTitleCase: "Automatic Assignment of Section Structure to Texts of Dutch Court Judgments",
+          correctionSentenceCase: "Automatic assignment of section structure to texts of dutch court judgments"
+      },
+      {
+        id: 5,
+        author: "Stallings, William",
+        title: "Computer Security Principles and Practice",
+        capitalization: "caseNotFound",
+        correctionTitleCase: "Computer Security Principles and Practice",
+        correctionSentenceCase: "Computer security principles and practice"
+      }
+    ]
   };
 
-  componentDidMount() {
-    BibtexAPI.get().then(stateServer => {
-      this.loadDataFromServer(stateServer);
-      console.log(stateServer);
-    });
-  }
+  // componentDidMount() {
+  //   BibtexAPI.get().then(stateServer => {
+  //     this.loadDataFromServer(stateServer);
+  //     console.log(stateServer);
+  //   });
+  // }
 
   getSelectedEntries = () =>
     this.state.entries.filter(entry =>
--- src/utils/MockData.js
+++ src/utils/MockData.js
@@ -4,70 +4,45 @@ const mockData = {
     {
       id: 1,
       author: "Milton Abramowitz and Irene A. Stegun",
-      title: "Handbook of mathematical Functions with Tables"
+      title: "Handbook of mathematical Functions with Tables",
+      capitalization: "caseNotFound",
+      correctionTitleCase: "Handbook of Mathematical Functions With Tables",
+      correctionSentenceCase: "Handbook of mathematical functions with tables"
     },
     {
       id: 2,
       author: "Bex, Floris and Villata, Serena",
       title:
-        "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference"
+        "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference",
+        capitalization: "sentenceCase",
+        correctionTitleCase: "Legal Knowledge and Information Systems: JURIX 2016: the Twenty-Ninth Annual Conference",
+        correctionSentenceCase: "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference"
     },
     {
       id: 3,
       author: "Winkels, Raboud Maarten",
-      title: "Section Structure of Dutch Court Judgments"
+      title: "Section Structure of Dutch Court Judgments",
+      capitalization: "titleCase",
+      correctionTitleCase: "Section Structure of Dutch Court Judgments",
+      correctionSentenceCase: "section structure of dutch court judgments"
     },
     {
       id: 4,
       author: "Trompper, Maarten and Winkels, Raboud",
       title:
-        "Automatic Assignment of Section Structure to Texts of Dutch Court Judgments"
+        "Automatic Assignment of Section Structure to Texts of Dutch Court Judgments",
+        capitalization: "titleCase",
+        correctionTitleCase: "Automatic Assignment of Section Structure to Texts of Dutch Court Judgments",
+        correctionSentenceCase: "Automatic assignment of section structure to texts of dutch court judgments"
     },
     {
       id: 5,
       author: "Stallings, William",
-      title: "Computer Security Principles and Practice"
+      title: "Computer Security Principles and Practice",
+      capitalization: "caseNotFound",
+      correctionTitleCase: "Computer Security Principles and Practice",
+      correctionSentenceCase: "Computer security principles and practice"
     }
-  ],
-  categories: {
-    capitalization: {
-      titleCase: [3, 4],
-      sentenceCase: [2],
-      caseNotFound: [1]
-    },
-    authorName: {},
-    missingRequiredFields: {
-    }
-  },
-  corrections: {
-    capitalization: [
-      {
-        id: 1,
-        correctionType: "TitleCase",
-        entryId: 1,
-        title: "Handbook of Mathematical Functions With Tables"
-      },
-      {
-        id: 2,
-        correctionType: "TitleCase",
-        entryId: 2,
-        title:
-          "Legal Knowledge and Information Systems: JURIX 2016: the Twenty-Ninth Annual Conference"
-      },
-      {
-        id: 3,
-        correctionType: "SentencesCase",
-        entryId: 1,
-        title: "Handbook of mathematical functions with tables"
-      },
-      {
-        id: 4,
-        correctionType: "SentencesCase",
-        entryId: 2,
-        title:
-          "Legal knowledge and information systems: JURIX 2016: the twenty-ninth annual conference"
-      }
-    ]
-  }
-};
+  ]
+}
 export default mockData;
